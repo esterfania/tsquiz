97214609fa0f7d8f12ac1e2c379e095f276d6623
Ester Gama <esterfaniasantos@gmail.com>
2021-01-25 18:38:20 -0300

    add meta tags

--- src/components/QuizHeader/index.tsx
+++ src/components/QuizHeader/index.tsx
@@ -5,15 +5,28 @@ export function QuizHeader({ props }: any) {
     <Head>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta charSet="UTF-8" />
-      <meta property="og:image" content={props.bg} />
-      <meta property="og:image:type" content="image/jpeg" />
-      <meta property="og:image:width" content="800" />
-      <meta property="og:image:height" content="600" />
-      <meta property="og:title" content={props.title} key="title" />
+
+      <title>{props.title}</title>
       <meta
         property="og:description"
         content="Dota 2 Quiz | Imersão React + Next Js Alura"
       />
+      <meta property="og:url" content="https://tsquiz.vercel.app/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={props.title} />
+
+      <meta property="og:image" content={props.bg} />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="https://tsquiz.vercel.app/" />
+      <meta property="twitter:url" content="https://tsquiz.vercel.app/" />
+      <meta name="twitter:title" content={props.title} />
+      <meta
+        name="twitter:description"
+        content="Dota 2 Quiz | Imersão React + Next Js Alura"
+      />
+      <meta name="twitter:image" content={props.bg} />
+
       <meta name="keywords" content="HTML, CSS, JavaScript" />
       <meta property="article:author" content={props.author} />
       <link rel="icon" type="image/png" href={props.icon} />
