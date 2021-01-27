0a2b68228e4d1044d3b0aa467f2ee38e966c973f
Ester Gama <esterfaniasantos@gmail.com>
2021-01-27 20:44:44 -0300

    feat(quiz): fix lint

--- pages/quiz.tsx
+++ pages/quiz.tsx
@@ -7,20 +7,20 @@ import QuizButton from '../src/components/QuizButton';
 export default function Quiz() {
   return (
     <>
-    <Widget>
-      <Widget.Header>
-        <BackButton />
-        <h3>Pergunta 1 de 2</h3>
-      </Widget.Header>
-      <Widget.Content>
-        <h2>Qual o nome da heroína da foto?</h2>
-        <QuizOptionButton>Drow Ranger</QuizOptionButton>
-        <QuizOptionButton>Mirana</QuizOptionButton>
-        <QuizOptionButton>Death Prophet</QuizOptionButton>
-        <QuizOptionButton>Queen of Pain</QuizOptionButton>
-        <QuizButton>Confirmar</QuizButton>
-      </Widget.Content>
-    </Widget>
+      <Widget>
+        <Widget.Header>
+          <BackButton />
+          <h3>Pergunta 1 de 2</h3>
+        </Widget.Header>
+        <Widget.Content>
+          <h2>Qual o nome da heroína da foto?</h2>
+          <QuizOptionButton>Drow Ranger</QuizOptionButton>
+          <QuizOptionButton>Mirana</QuizOptionButton>
+          <QuizOptionButton>Death Prophet</QuizOptionButton>
+          <QuizOptionButton>Queen of Pain</QuizOptionButton>
+          <QuizButton>Confirmar</QuizButton>
+        </Widget.Content>
+      </Widget>
     </>
   );
 }
